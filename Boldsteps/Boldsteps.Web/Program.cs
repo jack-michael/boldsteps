77dce8a7eddb97cb5105e769004f62d4992b7a10
jack-michael <jack@jackrogers.au>
2025-04-09 10:11:18 +0800

    Remove client project

--- Boldsteps/Boldsteps.Web/Program.cs
+++ Boldsteps/Boldsteps.Web/Program.cs
@@ -4,17 +4,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddInteractiveWebAssemblyComponents();
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -28,9 +23,6 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
-    .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(
-        typeof(Boldsteps.Shared._Imports).Assembly,
-        typeof(Boldsteps.Web.Client._Imports).Assembly);
+    .AddAdditionalAssemblies(typeof(Boldsteps.Shared._Imports).Assembly);
 
 app.Run();
